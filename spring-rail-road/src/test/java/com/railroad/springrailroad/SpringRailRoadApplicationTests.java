dc34440206313bb34fc5d2c4cf1149aaa48771a6
Oleh Sivak <olegsivak.2003@gmail.com>
2022-10-26 15:52:03 +0300

    Add template spring solution

---
+++ spring-rail-road/src/test/java/com/railroad/springrailroad/SpringRailRoadApplicationTests.java
@@ -0,0 +1,13 @@
+package com.railroad.springrailroad;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringRailRoadApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
