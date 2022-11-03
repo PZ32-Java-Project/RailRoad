d84abc104ee32905933633e0a0a633486860b8ef
Oleh Sivak <olegsivak.2003@gmail.com>
2022-11-03 11:26:15 +0100

    Add abstractions

---
+++ spring-rail-road/src/main/java/abstractions/Position.java
@@ -0,0 +1,25 @@
+package abstractions;
+
+public abstract class Position {
+    private int x;
+    private int y;
+
+    protected Position(int x, int y) {
+        this.x = x;
+        this.y = y;
+    }
+
+    public void moveTo(int x, int y) {
+        this.x = x;
+        this.y = y;
+    }
+
+    public boolean isPosition(Position position) {
+        return this.x != position.x || this.y != position.y;
+    }
+
+    // TODO: move to a new manager?
+    public double getDistance(Position position) {
+        return Math.sqrt((this.x - position.x) * (this.x - position.x) + (this.y - position.y) * (this.y - position.y));
+    }
+}
