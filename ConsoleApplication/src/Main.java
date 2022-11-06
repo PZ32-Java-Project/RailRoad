3eaae83d87f72158a75530e551e8a45c91c343dd
Brain-Ded <72191534+Brain-Ded@users.noreply.github.com>
2022-11-06 17:11:46 +0200

    Доробив сідери

--- ConsoleApplication/src/Main.java
+++ ConsoleApplication/src/Main.java
@@ -2,7 +2,5 @@ import models.Hall;
 
 public class Main {
     public static void main(String[] args) {
-        var hall = new Hall();
-        hall.initialize(4,4);
     }
 }
--- ConsoleApplication/src/models/Hall.java
+++ ConsoleApplication/src/models/Hall.java
@@ -2,16 +2,16 @@ package models;
 
 import abstractions.IInputManager;
 import abstractions.ISeedingManager;
-import abstractions.Position;
 import managers.InputManager;
 import managers.SeedingManager;
 import shared.Constants;
 import threaded.ClientServer;
 import threaded.ClientsSpawner;
 
-import java.util.List;
+import java.util.concurrent.locks.Lock;
+import java.util.concurrent.locks.ReentrantLock;
 
-import static shared.Constants.LINE_MAX_CLIENTS_COUNT;
+import static shared.Constants.*;
 
 // TODO: add storage (List) for lines, etc?
 public class Hall {
@@ -24,42 +24,38 @@ public class Hall {
         inputManager = new InputManager();
         seedingManager = new SeedingManager();
     }
-    public void spawnClients(){
-        var thread = new ClientsSpawner(this);
-        thread.start();
-/*        int i=0;
-        boolean enableSpawning = true;
-        while(i<100) {
-            int clientsCount = map.getClients().size();
-            //int clientsLimit = LINE_MAX_CLIENTS_COUNT*map.getCashRegistries().size()
-            int clientsLimit = LINE_MAX_CLIENTS_COUNT * 2;
-            if (clientsCount >= clientsLimit)
-                enableSpawning=false;
-            else if(clientsCount<clientsLimit*0.7)
-                enableSpawning = true;
-            if (enableSpawning) {
-                var client = SeedingManager.generateClient();
-                map.getPositions().add(client);
-                System.out.println("client " + client.getName() +" spawned at: "+ client.getPosition().getX()+","+client.getPosition().getY());
-            }
-            i++;
-        }*/
+
+    public void initialize(int cashCount, int entranceCount){
+        cashRegistriesCount=cashCount;
+        Constants.entranceCount =entranceCount;
+        Lock lock = new ReentrantLock();
+        generateCashRegistries();
+        generateCashEntrances();
+        spawnClients(lock);
+        initializeServing(lock);
     }
-    public void generateCashRegistries(int count){
-        var cashRegistries =  seedingManager.generateCashRegistries(count);
+    public void generateCashRegistries(){
+        var cashRegistries =  seedingManager.generateCashRegistries(cashRegistriesCount);
         map.getPositions().addAll(cashRegistries);
     }
-    public void initializeServing() {
+    public void generateCashEntrances(){
+        var entrances =  seedingManager.generateEntrances(entranceCount);
+        map.getPositions().addAll(entrances);
+    }
+    public void spawnClients(Lock lock){
+        var thread = new ClientsSpawner(this); // -1 Для рандомного інтервалу спавна, інакше в мілісекундах
+        thread.start();
+    }
+    public void initializeServing(Lock lock) {
 
         for (var cashRegistry :map.getCashRegistries() ) {
-            var thread = new ClientServer((CashRegistry)cashRegistry ,this);
+            var thread = new ClientServer((CashRegistry)cashRegistry ,this, lock, -1); // -1 Для рандомного інтервалу спавна, інакше в мілісекундах
             thread.start();
         }
-
     }
     public void setDataFromUserInput() {
         Constants.entranceCount = inputManager.getEntranceCount();
-        Constants.cashRegistriesCount = inputManager.getCashRegistriesCount();
+        cashRegistriesCount = inputManager.getCashRegistriesCount();
         Constants.cashRegistryServeTimeMax = inputManager.getCashRegistryServeTime();
     }
 
